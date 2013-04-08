a0b4bc845c8c7a8665386294355586753b31b817
ich <adverbmail@web.de>
2013-04-08 15:39:04 +0200

    Rechtschreibkorrektur die 2.

--- src/jea/Population.java
+++ src/jea/Population.java
@@ -57,7 +57,7 @@ public class Population {
 			children.calcFitness();
 			currentGeneration++;
 			generation = generation.getNextGeneration(children);
-			System.out.println("fetig");
+			System.out.println("fertig");
 		}
 	}
 
