83343e49799f3d0c11a718f66cd7b940993144da
ich <adverbmail@web.de>
2013-04-08 15:54:05 +0200

    Klasse Selection erstellt

---
+++ src/jea/Selection.java
@@ -0,0 +1,30 @@
+package jea;
+
+public class Selection {
+	
+	public static Generation determSelection(Generation generation){
+		
+		return generation;
+	}
+	
+	public static Generation probSelection(Generation generation){
+		
+		return generation;
+	}
+
+	public static Generation rouletteSelection(Generation generation){
+	
+		return generation;
+	}
+	
+	public static Generation qSelection(Generation generation){
+		
+		return generation;
+	}
+	
+	public static Generation multibleQSelection(Generation generation){
+		
+		return generation;
+	}
+
+}
